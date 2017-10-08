6ccf7c5999497ed96f58e6d49d6a724aab06a95a
Elias <mail@eliaszerrouq.nl>
2017-10-08 21:33:49 +0200

    fix spacing

--- TradeRouteMarketeer/Models/Market/Trade.cs
+++ TradeRouteMarketeer/Models/Market/Trade.cs
@@ -8,8 +8,11 @@ namespace TradeRouteMarketeer.Models.Market
     public class Trade
     {
         public string Commodity { get; set; }
+        
         public string Outlet { get; set; }
+        
         public int TradePrice { get; set; }
+        
         public int Amount { get; set; }
 
         public Trade(string commodity, string outlet, int tradePrice, int amount)
